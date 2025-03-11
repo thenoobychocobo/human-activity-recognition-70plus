13e6cca13cdce0d7cab50f7eada63beec7a059f0
Rylee <rjhlee123@gmail.com>
2025-03-11 17:30:29 +0800

    Updated saving code and added tracking of validation loss history

--- src/train_eval.py
+++ src/train_eval.py
@@ -19,18 +19,26 @@ def train_HAR70_model(
     model: nn.Module,
     optimizer: torch.optim.Optimizer,
     train_dataloader: DataLoader,
-    validation_dataloader: Optional[DataLoader],
+    validation_dataloader: DataLoader,
     num_epochs: int = 15,
     base_dir: str = "models",
     verbose: bool = True
 ):    
     criterion = nn.CrossEntropyLoss()
-    loss_history, accuracy_history, f1_history, precision_history, recall_history = [], [], [], [], []
+    training_loss_history, validation_loss_history, accuracy_history, f1_history, precision_history, recall_history = [], [], [], [], [], []
+    
+    # Create subdirectory to save models to during training session
+    os.makedirs(base_dir, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # Use timestamp as subdirectory name
+    model_type = type(model).__name__
+    subdirectory_name = f"{model_type}_{timestamp}"
+    save_dir = os.path.join(base_dir, subdirectory_name) # Subdirectory
+    os.makedirs(save_dir, exist_ok=True)
     
     for epoch in range(num_epochs):
         epoch += 1 # Account for zero-indexing
         start_time = time.time()
-        epoch_loss = 0
+        training_loss = 0
         model.train() # Set model to training mode
         
         for batch in train_dataloader:
@@ -44,7 +52,7 @@ def train_HAR70_model(
             # Forward pass
             pred_logits = model(sequence_batch)
             loss = criterion(pred_logits, labels_batch)
-            epoch_loss += loss.item()
+            training_loss += loss.item()
             
             # Backward pass            
             loss.backward()
@@ -52,53 +60,59 @@ def train_HAR70_model(
             
         # After each epoch
         # 1) Save the total loss for the epoch to loss_history
-        loss_history.append(epoch_loss)
-        # 2) Evaluate model on validation set (if provided)
-        if validation_dataloader:
-            accuracy, f1, precision, recall, conf_matrix = evaluate_HAR70_model(model, validation_dataloader)
-            accuracy_history.append(accuracy)
-            f1_history.append(f1)
-            precision_history.append(precision)
-            recall_history.append(recall)
+        training_loss_history.append(training_loss) # TODO: have training loss be average instead of total
+        # 2) Evaluate model on validation set
+        validation_loss, accuracy, f1, precision, recall, conf_matrix = evaluate_HAR70_model(model, validation_dataloader)
+        validation_loss_history.append(validation_loss)
+        accuracy_history.append(accuracy)
+        f1_history.append(f1)
+        precision_history.append(precision)
+        recall_history.append(recall)
             
         end_time = time.time()
         epoch_time = end_time - start_time
         
         if verbose:
             print(f"Epoch [{epoch}/{num_epochs}] | Time: {epoch_time:.2f}s")
-            print(f"Training Loss: {epoch_loss:.4f}")
-            if validation_dataloader: 
-                print(f"Accuracy: {accuracy:.4f}, F1: {f1:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}")
+            print(f"(Training) Loss: {training_loss:.4f}")
+            print(f"(Validation) Loss: {validation_loss:.4f}, Accuracy: {accuracy:.4f}, F1: {f1:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}\n")
                 
         # Save model every 5 epochs
         if epoch % 5 == 0:
-            save_model(model, epoch, base_dir=base_dir, verbose=verbose)
+            # Create base directory if it does not exist
+            os.makedirs(base_dir, exist_ok=True)
+            save_model(model, epoch, save_dir, verbose=verbose)
 
-    return loss_history, accuracy_history, f1_history, precision_history, recall_history, conf_matrix
+    return training_loss_history, validation_loss_history, accuracy_history, f1_history, precision_history, recall_history, conf_matrix
 
 def evaluate_HAR70_model(
     model, 
-    val_dataloader: DataLoader
+    validation_dataloader: DataLoader
 ):
     model.eval() # Set model to evaluation mode
+    criterion = nn.CrossEntropyLoss()
+    validation_loss = 0
+    
     accuracy = torchmetrics.classification.MulticlassAccuracy(num_classes=7).to(model.device)
     f1_score = torchmetrics.classification.MulticlassF1Score(num_classes=7, average="macro").to(model.device)
     precision = torchmetrics.classification.MulticlassPrecision(num_classes=7, average="macro").to(model.device)
     recall = torchmetrics.classification.MulticlassRecall(num_classes=7, average="macro").to(model.device)
     confusion_matrix = torchmetrics.ConfusionMatrix(num_classes=7, task="multiclass").to(model.device)
     with torch.no_grad():  # No gradients needed for evaluation
-        for inputs, targets in val_dataloader:
-            inputs, targets = inputs.to(model.device), targets.to(model.device)
+        for input_sequences, target_labels in validation_dataloader:
+            input_sequences, target_labels = input_sequences.to(model.device), target_labels.to(model.device)
 
-            outputs = model(inputs)  # Forward pass
-            predictions = torch.argmax(outputs, dim=1)  # Convert logits to class labels
+            logits = model(input_sequences)  # Forward pass
+            predictions = torch.argmax(logits, dim=1)  # Convert logits to class labels
 
             # Update metrics
-            accuracy.update(predictions, targets)
-            f1_score.update(predictions, targets)
-            precision.update(predictions, targets)
-            recall.update(predictions, targets)
-            confusion_matrix.update(predictions, targets)
+            loss = criterion(logits, target_labels)
+            validation_loss += loss.item()
+            accuracy.update(predictions, target_labels)
+            f1_score.update(predictions, target_labels)
+            precision.update(predictions, target_labels)
+            recall.update(predictions, target_labels)
+            confusion_matrix.update(predictions, target_labels)
 
     # Compute final metric values
     final_accuracy = accuracy.compute().item()
@@ -107,34 +121,22 @@ def evaluate_HAR70_model(
     final_recall = recall.compute().item()
     final_conf_matrix = confusion_matrix.compute().cpu().numpy() 
 
-    return final_accuracy, final_f1, final_precision, final_recall, final_conf_matrix
+    return validation_loss, final_accuracy, final_f1, final_precision, final_recall, final_conf_matrix
 
-def save_model(model, epoch, base_dir = "models", verbose: bool = True):
-    # Create base directory if it does not exist
-    os.makedirs(base_dir, exist_ok=True)
-    
-    # Create subdirectory containing saved models from training session
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # Use timestamp as subdirectory name
+def save_model(model, epoch, save_dir, verbose: bool = True):    
     model_type = type(model).__name__
-    subdirectory_name = f"{model_type}_{timestamp}"
-    save_dir = os.path.join(base_dir, timestamp)
-    os.makedirs(save_dir, exist_ok=True)
-    
-    model_type = type(model).__name__
-    model_filename = f"epoch_{epoch}.pth"
+    model_filename = f"{model_type}_epoch{epoch}.pth"
     model_path = os.path.join(save_dir, model_filename)
     torch.save(model.state_dict(), model_path)
     if verbose: print(f"✅ Model saved: {model_path}")
     
-def save_training_plots(loss_history, accuracy_history, f1_history, precision_history, recall_history, base_dir = "results"):
-    epochs = range(1, len(loss_history) + 1)  
-
-
-    os.makedirs(base_dir, exist_ok=True)
+def save_training_plots(training_loss_history, validation_loss_history, accuracy_history, f1_history, precision_history, recall_history, base_dir = "results"):
+    epochs = range(1, len(training_loss_history) + 1)  
+    os.makedirs(base_dir, exist_ok=True) # Directory to save plots in
 
     # Plot Training Loss
     plt.figure(figsize=(10, 5))
-    plt.plot(epochs, loss_history, label="Training Loss", color="red", marker="o")
+    plt.plot(epochs, training_loss_history, label="Training Loss", color="red", marker="o")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.title("Training Loss Over Epochs")
